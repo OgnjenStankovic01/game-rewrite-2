60664a664dc8dcc884fad4175e60063a113c35c0
Flatbaby <60462768+Flatbaby@users.noreply.github.com>
2024-07-25 15:31:41 +0200

    Newest version

--- src/main/kotlin/Creature.kt
+++ src/main/kotlin/Creature.kt
@@ -1,6 +1,6 @@
 open class Creature (var hp : Int, var xp : Int, var attack : Int, var level : Int) {
     fun attack(creature: Creature){
-        creature.hp =- attack
-        println("Attacked for $attack, remaining hp $creature.hp")
+        creature.hp =- this.attack
+        println("Attacked for $attack, remaining hp ${creature.hp}")
     }
 }
